05567eaa5a42f1fb8b7f1dd3a766c25f5b07d4d1
sebastienDOC <s.dechand21@gmail.com>
2024-12-22 21:05:20 +0100

    feat(landing-page): add about content

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { ServicesComponent } from "../../components/services/services.component";
+import { AboutComponent } from "../../components/about/about.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HeaderComponent, ServicesComponent],
+  imports: [HeaderComponent, ServicesComponent, AboutComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
