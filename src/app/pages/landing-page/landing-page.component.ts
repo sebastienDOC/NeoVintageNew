60a99c2d49d734dc12232fae38ebbc05e2d945ee
sebastienDOC <s.dechand21@gmail.com>
2024-12-24 19:10:24 +0100

    wip: add galery and start styling

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../../components/about/about.component";
 import { HeaderComponent } from "../../components/header/header.component";
 import { ServicesComponent } from "../../components/services/services.component";
-import { AboutComponent } from "../../components/about/about.component";
+import { PhotoGalleryComponent } from "../../components/photo-gallery/photo-gallery.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HeaderComponent, ServicesComponent, AboutComponent],
+  imports: [HeaderComponent, ServicesComponent, AboutComponent, PhotoGalleryComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
